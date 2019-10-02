acfcb29b7a0d9ce89feb910367875d7bcd67b04c
AnastasiiaSt <55129889+AnastasiiaSt@users.noreply.github.com>
2019-10-03 00:47:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrFlat = [];
+  let str1 = '';
+
+  for (let k = 0; k < bracketsConfig.length; k++) {
+    for (let l = 0; l < bracketsConfig[k].length; l++) {
+        str1 = str1.concat(bracketsConfig[k][l]);
+    }
+        arrFlat.push(str1);
+        str1 = '';
+   }
+
+  let result = 0;
+  let index;
+
+  if (str.length % 2 !== 0 ) {
+    return false;  
+  } else {
+
+    let i = 0;
+    let j = 0;
+    let n = str.length;
+
+    do {
+      index = str.indexOf(arrFlat[i]);
+
+      if (index === -1) {
+        result += 0;
+      } else {
+        str = str.replace(arrFlat[i], '');
+        result +=1
+      } 
+
+        j = j + 1;
+              
+      if (result === (n / 2) || str === '') {
+        break;
+      } else if (i >= arrFlat.length - 1 ) {
+        i = 0;
+      } else {
+        i = i + 1;
+      }
+    } while (j <= n * arrFlat.length);
+
+    if (result === (n / 2)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
